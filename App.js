584d09c8541b501d4ae03c0a38bc464d57a77213
arsenelanang <arsenelanang@gmail.com>
2017-12-10 01:23:17 +0800

    uji coba

---
+++ App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+} from 'react-native';
+
+
+
+
+
+export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Useless Placeholder' };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native Lanang Trisna tamvan!
+        </Text>
+        <TextInput
+        style={styles.textcoba}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  textcoba: {
+    height: 40, 
+    borderColor: 'red', 
+    borderWidth: 1,
+  }
+});
